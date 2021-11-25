a020cacf34f68d21b66ec4e4f8a4b8a8edff26a4
DariaOni <daria.oni@outlook.com>
2021-11-25 16:01:37 +0200

    broken function

---
+++ file.js
@@ -0,0 +1,13 @@
+function lastLetter(string) {
+	str = string.split(' ');
+
+	for (i = 0; i < str.length; i++) {
+		str[i] = str[i].charAt(str[i].length - 1).toUpperCase();
+	}
+
+	return str.join(' ');
+}
+
+let rainbow = 'Richard of york gave battle in vain';
+
+lastLetter(rainbow);
