09de4b1e6472e3e92445852fd22e714d5bb6f38f
DariaOni <daria.oni@outlook.com>
2021-11-29 16:15:52 +0200

    checking for letters

--- file.js
+++ file.js
@@ -1,28 +1,33 @@
 function lastLetter(string) {
-	if (checkLastLetter(string) === true) {
-		return;
-	}
 	let str = string.split(' ');
 	// ['Richard', 'of', 'york', 'gave', 'battle', 'in', 'vain']
 	const arrLength = str.length;
+
 	for (i = 0; i < arrLength; i++) {
-		const strLength = str[i].length;
-		str[i] = str[i].slice(0, strLength - 1) + str[i].slice(-1).toUpperCase();
+		if (checkLastLetter(str[i])) {
+			const strLength = str[i].length;
+			str[i] = str[i].slice(0, strLength - 1) + str[i].slice(-1).toUpperCase();
+		}
 	}
 
 	return str.join(' ');
 }
 
-function checkLastLetter(string) {
-	let symbols = '~`!#$%^&*+=-[]\\\';,/{}|":<>?';
-	let lastEl = string.charAt(string.length - 1);
+function checkLastLetter(str) {
+	console.log(`checkLastLetter param: ${str}`);
 
+	let symbols = 'abcdefghijklmnopqrstuvwxyz'; // ABC
+	let lastEl = str.charAt(str.length - 1);
+	console.log(`checkLastLetter lastEl: ${lastEl}`);
 	if (symbols.includes(lastEl)) {
+		console.log(`checkLastLetter symbols.includes(lastEl): true}`);
 		return true;
 	} else {
+		console.log(`checkLastLetter symbols.includes(lastEl): false}`);
 		return false;
 	}
 }
-let rainbow = 'Richard, of york gave battle in vain';
 
-lastLetter(rainbow);
+let rainbow = 'Richard, of york- gave: battle in[ vain .';
+
+console.log(lastLetter(rainbow));
